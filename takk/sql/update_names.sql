df6ee8f63783217f7305b2555b34f3bfb4531801
nabassa1323 <132025448+nabassa1323@users.noreply.github.com>
2026-01-30 15:14:56 +0100

    Delete takk directory

--- takk/sql/update_names.sql
+++
@@ -1,6 +0,0 @@
--- Mise à jour des noms : Bébé So → Solange, Matt → Mathieu
-
-USE c1498480c_mariage;
-
-UPDATE utilisateurs SET nom = 'Solange' WHERE id = 1;
-UPDATE utilisateurs SET nom = 'Mathieu' WHERE id = 2;
